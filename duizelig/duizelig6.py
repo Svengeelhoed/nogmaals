13bbeb4e40517aad6aff34a4926b6f82190dbdea
Svengeelhoed <99069588@mydavinci.nl>
2021-10-05 09:42:09 +0200

    duizelig 6 gemaakt

--- duizelig/duizelig6.py
+++ duizelig/duizelig6.py
@@ -1,4 +1,16 @@
-i=50
-f = "ok"
-while i + i <= 1000:
-    f = print(i + i(+1))
+x = 50
+y = 51
+ans = 0 
+# while ans <= 1000:
+#     ans = ans + x + y
+#     print(str(ans) + "+" + str(x) + " + " + str(y) + " = " + str(ans))
+#     y = y + 2
+#     x = x + 2
+
+while ans <= 1000:
+    ans = x + ans
+    print(str(ans) + "+" + str(x) + "=" + str(ans))
+    x = x + 1
+
+
+
